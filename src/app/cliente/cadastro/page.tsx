771b562f3e6e4678a9cbf6724b91ab152a5fc6ce
Henrique Vidotto <henriquevidotto7@hotmail.com>
2026-05-30 19:02:57 -0300

    correçõess

--- src/app/cliente/cadastro/page.tsx
+++ src/app/cliente/cadastro/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import FormClienteTemplate from "@/components/templates/FormClienteTemplate";
 
+
 export default function Cadastro() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
